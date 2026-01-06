47411e0a6dfb97733889c70e6b7317b2d007e1cd
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-01-06 16:00:49 +0000

    Replace autogenerated paths in ts to real ones

--- i18n/u-scope_en.ts
+++ i18n/u-scope_en.ts
@@ -2,57 +2,65 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>DataProcessor</name>
+    <message>
+        <location filename="../source/dataprocessor.cpp" line="134"/>
+        <source>Reader [#%1] state has been updated to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="130"/>
+        <location filename="../source/mainwindow.ui" line="20"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="131"/>
-        <source>Config UART</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="132"/>
+        <location filename="../source/mainwindow.ui" line="66"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="133"/>
+        <location filename="../source/mainwindow.ui" line="73"/>
         <source>Stop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="134"/>
+        <location filename="../source/mainwindow.ui" line="108"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="135"/>
+        <location filename="../source/mainwindow.ui" line="113"/>
         <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="123"/>
+        <source>Config UART</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>UartConfigDialog</name>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="108"/>
+        <location filename="../source/uartconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>UART port config</translation>
+        <translation>UART Port config</translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="110"/>
+        <location filename="../source/uartconfigdialog.ui" line="29"/>
         <source>Manufacturer:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="111"/>
+        <location filename="../source/uartconfigdialog.ui" line="36"/>
         <source>Description:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="112"/>
+        <location filename="../source/uartconfigdialog.ui" line="43"/>
         <source>Serial number:</source>
         <translation type="unfinished"></translation>
     </message>

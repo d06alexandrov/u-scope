ce88154e3bdf2b4a98e930c2531ee6cdfcdd6211
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-01-06 15:07:44 +0000

    Add translation (linguist) support

---
+++ i18n/u-scope_en.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="130"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="131"/>
+        <source>Config UART</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="132"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="133"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="134"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_mainwindow.h" line="135"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UartConfigDialog</name>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="108"/>
+        <source>Dialog</source>
+        <translation>UART port config</translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="110"/>
+        <source>Manufacturer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="111"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../build/source/u-scope_autogen/include/ui_uartconfigdialog.h" line="112"/>
+        <source>Serial number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

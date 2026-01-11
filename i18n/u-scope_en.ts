f856d08c4322ec2b0e8c25b2303bb1a38c9e7d21
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-01-11 19:54:32 +0000

    Update translations

--- i18n/u-scope_en.ts
+++ i18n/u-scope_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>DataProcessor</name>
     <message>
-        <location filename="../source/dataprocessor.cpp" line="134"/>
+        <location filename="../source/dataprocessor.cpp" line="175"/>
         <source>Reader [#%1] state has been updated to %2</source>
         <translation type="unfinished"></translation>
     </message>
@@ -17,58 +17,139 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../source/mainwindow.ui" line="66"/>
+        <location filename="../source/mainwindow.ui" line="37"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="50"/>
+        <source>Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="81"/>
+        <location filename="../source/mainwindow.ui" line="170"/>
+        <source>CH1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="88"/>
+        <location filename="../source/mainwindow.ui" line="219"/>
+        <source>CH2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="146"/>
+        <source>Control panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="155"/>
+        <source>Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="163"/>
+        <source>CH10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="177"/>
+        <source>CH6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="184"/>
+        <source>CH11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="191"/>
+        <source>CH3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="198"/>
+        <source>CH7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="205"/>
+        <source>CH5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="212"/>
+        <source>CH9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="226"/>
+        <source>CH4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="233"/>
+        <source>CH8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="240"/>
+        <source>CH12</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="251"/>
+        <location filename="../source/mainwindow.ui" line="305"/>
+        <source>Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="272"/>
+        <location filename="../source/mainwindow.ui" line="326"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="297"/>
+        <source>Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="361"/>
+        <source>Trigger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="367"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../source/mainwindow.ui" line="73"/>
+        <location filename="../source/mainwindow.ui" line="374"/>
         <source>Stop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../source/mainwindow.ui" line="108"/>
+        <location filename="../source/mainwindow.ui" line="401"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../source/mainwindow.ui" line="113"/>
-        <source>Options</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../source/mainwindow.ui" line="123"/>
+        <location filename="../source/mainwindow.ui" line="408"/>
         <source>Config UART</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>UartConfigDialog</name>
-    <message>
-        <location filename="../source/uartconfigdialog.ui" line="14"/>
-        <source>Dialog</source>
-        <translation>UART Port config</translation>
-    </message>
-    <message>
-        <location filename="../source/uartconfigdialog.ui" line="29"/>
-        <source>Manufacturer:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../source/uartconfigdialog.ui" line="36"/>
-        <source>Description:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../source/uartconfigdialog.ui" line="43"/>
-        <source>Serial number:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>UniversalReader</name>
     <message>
-        <location filename="../source/universalreader.cpp" line="42"/>
+        <location filename="../source/universalreader.cpp" line="31"/>
+        <source>Caught error during reader initialization:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/universalreader.cpp" line="54"/>
         <source>Caught error:</source>
         <translation type="unfinished"></translation>
     </message>

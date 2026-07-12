f3589b5f367a189326f2a4ee1606e2e01fdf6489
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-07-12 20:45:59 +0000

    Add doxygen config file

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -3,6 +3,11 @@ find_package(Doxygen)
 
 if(Doxygen_FOUND)
     set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/doxygen)
+    set(DOXYGEN_INPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/source)
 
-    doxygen_add_docs(doxygen_docs ${CMAKE_CURRENT_SOURCE_DIR}/source)
+    configure_file(
+        ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_BINARY_DIR}/Doxyfile
+    )
+
+    doxygen_add_docs(doxygen_docs CONFIG_FILE ${CMAKE_BINARY_DIR}/Doxyfile)
 endif()

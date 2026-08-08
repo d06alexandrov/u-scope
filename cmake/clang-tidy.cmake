ac8a114d857b5510698d79a173f8e875e8a71053
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-08-08 22:02:13 +0000

    Add clang-tidy config file

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,8 +1,8 @@
 # Clang-tidy checks
 find_program(CLANG_TIDY clang-tidy)
 
-if(CLANG_TIDY)
+if(CLANG_TIDY AND NOT "$ENV{CI}")
     set(CMAKE_CXX_CLANG_TIDY
-        "${CLANG_TIDY};--checks=-*,cppcoreguidelines-*,modernize-*"
+        "${CLANG_TIDY};--config-file=${CMAKE_SOURCE_DIR}/.clang-tidy"
     )
 endif()

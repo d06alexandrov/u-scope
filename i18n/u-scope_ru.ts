61b86fd9dbc25f667f76627a6890ad8972fc5ab9
Dmitriy Alexandrov <d06alexandrov@gmail.com>
2026-09-12 15:47:42 +0000

    Add russian translation

---
+++ i18n/u-scope_ru.ts
@@ -0,0 +1,444 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>AppController</name>
+    <message>
+        <location filename="../source/ui/controllers/appcontroller.cpp" line="39"/>
+        <source>About %1</source>
+        <translation>О программе %1</translation>
+    </message>
+    <message>
+        <location filename="../source/ui/controllers/appcontroller.cpp" line="40"/>
+        <source>&lt;h3&gt;%1&lt;/h3&gt;&lt;p&gt;Version %2&lt;/p&gt;&lt;p&gt;Built with Qt %3&lt;/p&gt;This program is free software released under the GNU General Public License.</source>
+        <translation>&lt;h3&gt;%1&lt;/h3&gt;&lt;p&gt;Версия %2&lt;/p&gt;&lt;p&gt;Собрано с Qt %3&lt;/p&gt;Эта программа является свободным программным обеспечением; вы можете распространять ее и/или изменять ее на условиях GNU General Public License.</translation>
+    </message>
+    <message>
+        <location filename="../source/ui/controllers/appcontroller.cpp" line="162"/>
+        <source>Failed to initialize Data Processor.</source>
+        <translation>Не удалось инициализировать Data Processor.</translation>
+    </message>
+</context>
+<context>
+    <name>DataProcessor</name>
+    <message>
+        <location filename="../source/core/dataprocessor.cpp" line="46"/>
+        <source>Reader [#%1] state has been updated to %2</source>
+        <translation>Источник [#%1] перешел в состояние %2</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../source/ui/qml/MainWindow.qml" line="11"/>
+        <source>U-Scope</source>
+        <translation>U-Scope</translation>
+    </message>
+    <message>
+        <location filename="../source/ui/qml/MainWindow.qml" line="19"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Помощь</translation>
+    </message>
+    <message>
+        <location filename="../source/ui/qml/MainWindow.qml" line="21"/>
+        <source>&amp;About</source>
+        <translation>&amp;О программе</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewChart</name>
+    <message>
+        <location filename="../source/ui/qml/OverviewChart.qml" line="16"/>
+        <source>Measurements history is empty.</source>
+        <translation>История принятых данных пуста.</translation>
+    </message>
+</context>
+<context>
+    <name>PacketFieldDialog</name>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="26"/>
+        <source>Packet Field</source>
+        <translation>Поле пакета</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="61"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="66"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="82"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="87"/>
+        <source>e.g. Voltage</source>
+        <translation>например Напряжение</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="91"/>
+        <source>Offset (bytes)</source>
+        <translation>Отступ (в байтах)</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="102"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="111"/>
+        <source>Endianness</source>
+        <translation>Знаковость</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/PacketFieldDialog.qml" line="125"/>
+        <source>Field extends past the packet length.</source>
+        <translation>Поле выходит за границы пакета.</translation>
+    </message>
+</context>
+<context>
+    <name>ReaderDialog</name>
+    <message>
+        <location filename="../source/readers/qml/ReaderDialog.qml" line="17"/>
+        <source>Modify</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/qml/ReaderDialog.qml" line="17"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/qml/ReaderDialog.qml" line="21"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>SerialReader</name>
+    <message>
+        <location filename="../source/readers/serial/serialreader.cpp" line="57"/>
+        <source>Baud rate must be equal or greater than %1</source>
+        <translation>Скорость передачи данных (Baudrate) должна быть не меньше, чем %1</translation>
+    </message>
+</context>
+<context>
+    <name>SerialReaderDialog</name>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="13"/>
+        <source>Serial Port Configuration</source>
+        <translation>Настройка последовательного порта</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="72"/>
+        <source>Device:</source>
+        <translation>Устройство:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="86"/>
+        <source>Baudrate:</source>
+        <translation>Скорость (бод/с):</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="106"/>
+        <source>Data Bits:</source>
+        <translation>Биты данных:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="140"/>
+        <source>Parity:</source>
+        <translation>Бит четности:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="148"/>
+        <source>None</source>
+        <translation>Нет</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="152"/>
+        <source>Even</source>
+        <translation>Чёт</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="156"/>
+        <source>Odd</source>
+        <translation>Нечёт</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="160"/>
+        <source>Space</source>
+        <translation>Пробел</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="164"/>
+        <source>Mark</source>
+        <translation>Маркер</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="178"/>
+        <source>Stop bits:</source>
+        <translation>Стоп-биты:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="204"/>
+        <source>Flow control:</source>
+        <translation>Управление потоком:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="212"/>
+        <source>No Flow Control</source>
+        <translation>Отключено</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="216"/>
+        <source>Hardware Control</source>
+        <translation>Аппаратное</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="220"/>
+        <source>Software Control</source>
+        <translation>Программное</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="234"/>
+        <source>Data format:</source>
+        <translation>Формат данных:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="239"/>
+        <source>Single byte</source>
+        <translation>Единичные байты</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="239"/>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="295"/>
+        <source>Packet</source>
+        <translation>Пакет данных</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="252"/>
+        <source>Single Byte</source>
+        <translation>Единичный байт</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="258"/>
+        <source>Interprets each received byte as one sample.</source>
+        <translation>Интерпретировать каждый полученный байт как одно значение.</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="266"/>
+        <source>Signedness:</source>
+        <translation>Знаковость:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="269"/>
+        <source>Signed</source>
+        <translation>Знаковое</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="278"/>
+        <source>Unsigned</source>
+        <translation>Беззнаковое</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="308"/>
+        <source>Start magic (hex):</source>
+        <translation>Заголовок пакета (шестн.):</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="313"/>
+        <source>e.g. AD 57</source>
+        <translation>например AD 57</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="322"/>
+        <source>End magic (hex):</source>
+        <translation>Трейлер пакета (шестн.):</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="327"/>
+        <source>e.g. 90 EF (optional)</source>
+        <translation>например 90 EF (необязательное поле)</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="336"/>
+        <source>Packet length (bytes):</source>
+        <translation>Длина пакета (в байтах):</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="350"/>
+        <source>Fields:</source>
+        <translation>Поля:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="373"/>
+        <source>Edit</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="377"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/qml/SerialReaderDialog.qml" line="385"/>
+        <source>+ Add field</source>
+        <translation>+ Добавить новое поле</translation>
+    </message>
+</context>
+<context>
+    <name>SerialReaderDialogModel</name>
+    <message>
+        <location filename="../source/readers/serial/serialreader_dialog_model.cpp" line="55"/>
+        <source>Signed byte</source>
+        <translation>Байт со знаком</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/serial/serialreader_dialog_model.cpp" line="55"/>
+        <source>Unsigned byte</source>
+        <translation>Байт без знака</translation>
+    </message>
+</context>
+<context>
+    <name>SerialReaderModule</name>
+    <message>
+        <location filename="../source/readers/serial/serialreader_module.cpp" line="9"/>
+        <source>Serial source</source>
+        <translation>Последовательный порт</translation>
+    </message>
+</context>
+<context>
+    <name>SimulatedFormDialog</name>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="15"/>
+        <source>Simulated Form</source>
+        <translation>Симуляционная форма</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="48"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="52"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="71"/>
+        <source>Constant Value</source>
+        <translation>Константное значение</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="71"/>
+        <source>Sinusoidal Wave</source>
+        <translation>Синусоида</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="80"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="94"/>
+        <source>Amplitude</source>
+        <translation>Амплитуда</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedFormDialog.qml" line="104"/>
+        <source>Frequency (Hz)</source>
+        <translation>Частота (Гц)</translation>
+    </message>
+</context>
+<context>
+    <name>SimulatedReader</name>
+    <message>
+        <location filename="../source/readers/simulated/simulatedreader.cpp" line="28"/>
+        <source>Allowed sample rate is within the range [%1;%2]</source>
+        <translation>Допустимачастота дискретизации в диапазоне [%1;%2]</translation>
+    </message>
+</context>
+<context>
+    <name>SimulatedReaderDialog</name>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedReaderDialog.qml" line="13"/>
+        <source>Simulated Reader Configuration</source>
+        <translation>Конфигурация симуляционного источника</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedReaderDialog.qml" line="66"/>
+        <source>Edit</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedReaderDialog.qml" line="70"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/qml/SimulatedReaderDialog.qml" line="78"/>
+        <source>+ Add new form</source>
+        <translation>+ Добавить новую форму</translation>
+    </message>
+</context>
+<context>
+    <name>SimulatedReaderDialogConfig</name>
+    <message>
+        <location filename="../source/readers/simulated/simulatedreader_dialog.cpp" line="21"/>
+        <source>Constant %1</source>
+        <translation>Константа %1</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/simulated/simulatedreader_dialog.cpp" line="26"/>
+        <source>Sinusoid (%1 Amplitude) %2 Hz</source>
+        <translation>Синусоида (Амплитуда %1) %2 Гц</translation>
+    </message>
+</context>
+<context>
+    <name>SimulatedReaderModule</name>
+    <message>
+        <location filename="../source/readers/simulated/simulatedreader_module.cpp" line="9"/>
+        <source>Simulated source</source>
+        <translation>Симуляционный источник</translation>
+    </message>
+</context>
+<context>
+    <name>SourceList</name>
+    <message>
+        <location filename="../source/ui/qml/SourceList.qml" line="32"/>
+        <source>Add %1</source>
+        <translation>Добавить %1</translation>
+    </message>
+</context>
+<context>
+    <name>SourceListController</name>
+    <message>
+        <location filename="../source/ui/controllers/sourcelist_controller.cpp" line="79"/>
+        <source>Source %1</source>
+        <translation>Источник №%1</translation>
+    </message>
+    <message>
+        <location filename="../source/ui/controllers/sourcelist_controller.cpp" line="86"/>
+        <source>#%1 %2</source>
+        <translation>Значение №%1 %2</translation>
+    </message>
+</context>
+<context>
+    <name>UniversalReader</name>
+    <message>
+        <location filename="../source/readers/universalreader.cpp" line="26"/>
+        <source>Caught error during reader initialization:</source>
+        <translation>В процессе инициализации источника возникла ошибка:</translation>
+    </message>
+    <message>
+        <location filename="../source/readers/universalreader.cpp" line="49"/>
+        <source>Caught error:</source>
+        <translation>Возникла ошибка:</translation>
+    </message>
+</context>
+</TS>
